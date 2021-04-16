d57ee21082aa786e75addf56566eb4d01fa079bd
mihyeon <2_5_3_1@naver.com>
2021-04-16 16:05:55 +0900

    g

--- src/api/place/place.controller.js
+++ src/api/place/place.controller.js
@@ -2,10 +2,20 @@
 import { findAll, findOne } from "./place.repository.js";
 
 export async function index( req, res ){
-    const { success , message, data, error } = await findAll( );
+
+    const resData = { success:null, message:null, data:null, error:null}
+    if(!req.query.lat || !req.query.lon){
+        const { success , message, data, error } = await findAll( );
+        resData.success = success;
+        resData.message = message;
+        resData.data = data;
+        resData.error = error;
+    }
+    //const { place_code , lat, lon } = req.query;
+    
     success === true ? 
-    res.status(200).json({ message: message , data : data}) : 
-    res.status(500).json({ message: message , error : error });
+    res.status(200).json({ message: resData.message , data : resData.data}) : 
+    res.status(500).json({ message: resData.message , error : resData.error });
 } 
 
 export async function show( req, res ){
