d63241a9dfa29f7dc0b5eaf44183f1974c3e93ec
mihyeon <2_5_3_1@naver.com>
2021-04-16 16:52:44 +0900

    findAll update

--- src/api/place/place.controller.js
+++ src/api/place/place.controller.js
@@ -2,20 +2,12 @@
 import { findAll, findOne } from "./place.repository.js";
 
 export async function index( req, res ){
-
-    const resData = { success:null, message:null, data:null, error:null}
-    if(!req.query.lat || !req.query.lon){
-        const { success , message, data, error } = await findAll( );
-        resData.success = success;
-        resData.message = message;
-        resData.data = data;
-        resData.error = error;
-    }
-    //const { place_code , lat, lon } = req.query;
-    
+    const { place_code , lat, lon } = req.query;
+    console.log(place_code , lat, lon);
+    const { success , message, data, error } = await findAll(place_code, lat, lon);
     success === true ? 
-    res.status(200).json({ message: resData.message , data : resData.data}) : 
-    res.status(500).json({ message: resData.message , error : resData.error });
+    res.status(200).json({ message: message , data : data}) : 
+    res.status(500).json({ message: message , error : error });
 } 
 
 export async function show( req, res ){
@@ -27,10 +19,6 @@ export async function show( req, res ){
 }
 
 export async function create( req, res ) {
-    // console.log( " 요청 옴 ");
-    // console.log(req.body );
-    // console.log(req.files);
-
     const body = req.body;
     const { success , message, data, error } = await store( body );
     success === true ? 
--- src/api/place/place.repository.js
+++ src/api/place/place.repository.js
@@ -1,10 +1,35 @@
 "use strict"
 import { query } from "../../config/database.js";
-
-export async function findAll( ){
+ 
+export async function findAll(place_code, lat, lon ){
+    console.log("findAll");
     let SQL = `
-    select name, address, phone, lat, lon, add_info
-    from places`;
+    select name, address, phone, lat, lon, add_info`;
+    switch(place_code) {
+        case '1' : SQL += `
+            from places 
+            where place_code = 1
+            `;
+            break;
+        case '2' : SQL += `
+            from places 
+            where place_code = 2
+            `;
+            break;
+        case '5' : SQL += `
+            from places 
+            where place_code = 5
+            `;
+            break;
+        case '6' : SQL += `
+            from places 
+            where place_code = 6
+            `;
+            break;
+        default : null;
+    }
+    SQL += `order by  ST_DistanceSphere(geom, ST_MakePoint(lon,lat))`;
+    console.log(SQL);
     return query(SQL)
         .then( data => { 
             return { success : true, message : "finded successfully", data : data};
