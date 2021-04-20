da57bcccf21221314d824adef4375493a18d8105
Akrom <akobidov777@gmail.com>
2021-04-20 09:09:47 +0900

    adding check function

--- src/apis/auth/auth.controller.js
+++ src/apis/auth/auth.controller.js
@@ -1,75 +1,125 @@
 "use strict"
 
-import { comparePassword, encryptedPassowrd } from "../../utils/bcrypt.js";
-import { createToken } from "../../utils/jwt.js";
-import { findLoginInfo, store } from "./auth.repository.js";
+import {
+    comparePassword,
+    encryptedPassowrd
+} from "../../utils/bcrypt.js";
+import {
+    createToken
+} from "../../utils/jwt.js";
+import {
+    findLoginInfo,
+    store
+} from "./auth.repository.js";
 
-export async function signin( req, res ) {
-    const { email , password } = req.body;
-
+//login user
+export async function signin(req, res) {
+    const {
+        email,
+        password
+    } = req.body;
+    // no need password
     console.log(email, password);
     // find id, email, password from email
-    const { success , result } = await findLoginInfo( email );
+    const {
+        success,
+        result
+    } = await findLoginInfo(email);
 
     // query error
-    if( !success ) 
+    if (!success)
         return res.status(500).json({
-            message : "login false",
-            data : result  
+            message: "login false",
+            data: result
         });
     // not found email
-    if( !result ) 
+    if (!result)
         return res.status(400).json({
-            message : "not found email",
-            data : false
+            message: "not found email",
+            data: false
         });
-        
+
     // not matched password
-    const isMatched = comparePassword( password, result[0].password );
-    if(!isMatched) 
+    const isMatched = comparePassword(password, result[0].password);
+    if (!isMatched)
         return res.status(400).json({
-            message : "not matched password",
-            data : false
+            message: "not matched password",
+            data: false
         });
-    
+
     // 위의 유효성 검사들을 통과하면 토큰 발급 ( 토큰에는 유저의 id와 roles 가 들어감 )
-    const { id ,roles } = result[0];
+    const {
+        id,
+        roles
+    } = result[0];
     console.log(id);
-    const token = createToken( id );
+    const token = createToken(id);
     console.log(token);
-    res.status(200).json({ message : "login success" , data : token });
+    res.status(200).json({
+        message: "login success",
+        data: token
+    });
 }
 
-export async function signup( req, res ) {
-    const { email, password } = req.body;
+//create user
+export async function signup(req, res) {
+    const {
+        email,
+        nickname,
+        gender,
+        birthday,
+        age,
+        URL,
+        password
+    } = req.body;
     // find id, email, password from email
-    let resultObject = await findLoginInfo( email ); 
+    console.log(req.body);
+    let resultObject = await findLoginInfo(email);
 
     // query error 
-    if( !resultObject.success )
+    if (!resultObject.success)
         return res.status(500).json({
-            message : "sign-up false",
-            data : resultObject.result  
+            message: "sign-up false",
+            data: resultObject.result
         });
     // not found email
-    if( resultObject.result ) 
+    if (resultObject.result)
         return res.status(400).json({
-            message : "email duplicated",
-            data : false
+            message: "email duplicated",
+            data: false
         });
 
     // 비밀번호 암호화
-    const hsPassword = await encryptedPassowrd( password );
+    const hsPassword = await encryptedPassowrd(password);
     // 회원 저장
-    resultObject = await store( email, hsPassword );
+    resultObject = await store(email, nickname,gender,birthday,age,URL, hsPassword);
 
-    !resultObject.success ? 
-    res.status(500).json({
-        message : "user store false",
-        data : resultObject.result  
-    }) :
-    res.status(200).json({
-        message : "user stored",
-        data : resultObject.result
-    });
+    !resultObject.success ?
+        res.status(500).json({
+            message: "user store false",
+            data: resultObject.result
+        }) :
+        res.status(200).json({
+            message: "user stored",
+            data: resultObject.result
+        });
+}
+
+export async function check(req, res) {
+    const {
+        userId,
+        nickname
+    } = req.body;
+    let resultObject = userId === undefined ?
+        await findLoginInfo(userId, nickname) :
+        await findLoginInfo(userId, nickname);
+    return resultObject.success ? res.status(200).json({
+            message: "get results",
+            data: resultObject.result
+        }) :
+        res.status(500).json({
+            message: "Could not find",
+            console: resultObject.result
+        });
+
 }
--- src/apis/auth/auth.repository.js
+++ src/apis/auth/auth.repository.js
@@ -1,39 +1,59 @@
 "use strict"
 
-import { query } from "../../config/database.js";
+import {
+    query
+} from "../../config/database.js";
 
 // email을 검색하여 email, password 반환( 없으면 false 반환 )
-export async function findLoginInfo( email ) {
-    console.log( email );
-    let sql = `
-    select id, email, password
+export async function findLoginInfo(email, nickname) {
+    console.log(email, nickname);
+    let sql = email ? `
+    select id, email, password, nickname
     from users 
-    where email = '${email}'`;
+    where email = '${email}'` : `select  nickname
+    from users 
+    where nickname = '${nickname}'`;
 
     return await query(sql)
-        .then( data => {
-            console.log(data);
-            if( data.rowCount === 0 ) return  { success : true , result : false };
-            return { success : true , result : data.rows };
+        .then(data => {
+            //console.log(data);
+            if (data.rowCount === 0) return {
+                success: true,
+                result: false
+            };
+            return {
+                success: true,
+                result: data.rows
+            };
         })
-        .catch( error => {
+        .catch(error => {
             console.log(error);
-            return { success : false , result : error};
+            return {
+                success: false,
+                result: error
+            };
         });
 }
 
 // email, password 를 받아 유저 저장
-export async function store( email, password ){
+export async function store(email, nickname,gender,birthday,age,URL,  password) {
+    age = Number(age);
     let sql = `
     insert into
-    users(email, password)
-    values('${email}', '${password}')`;
-
+    users (password, profile_url, baby_gender, baby_birthday, parent_age,email,nickname)
+    values('${password}', '${URL}','${gender}','${birthday}','${age}','${email}','${nickname}');`;
+    console.log();
     return await query(sql)
-        .then( data => {
-            return { success : true , result : true };
+        .then(data => {
+            return {
+                success: true,
+                result: true
+            };
         })
-        .catch( error => {
-            return { success : false , result : error};
+        .catch(error => {
+            return {
+                success: false,
+                result: error
+            };
         });
 }
