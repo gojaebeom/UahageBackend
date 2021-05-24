829bfd93e30ae7dc976d550f964152e2a184b81e
mihyeon <2_5_3_1@naver.com>
2021-05-24 15:29:52 +0900

    order by lat lon

--- src/apis/places/hospital/placeController.js
+++ src/apis/places/hospital/placeController.js
@@ -3,7 +3,10 @@ const repository = require("./placeRepository");
 
 // 장소 리스트 보기
 exports.findByOptions = async (req, res) => {
-    const { success, result, error } = await repository.findByOptions(  );
+    console.log("findbyoptions");
+    console.log(req.query);
+    const {pageNumber, lat, lon} = req.query;
+    const { success, result, error } = await repository.findByOptions( pageNumber ,lat,lon);
     success ? 
     res.status(200).json({ message : "status ok",  data : result }) : 
     res.status(500).json({ message : "server error", error : error }); 
